76500fa026220e028550dc704045d0325ebe508c
Christina Michaels <christinamichaels@Christinas-MacBook-Pro-2.local>
2020-10-06 16:09:31 -0400

    Docker changes during class on Oct. 6

--- contacts.js
+++ contacts.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var app = express();
 const fetch = require("node-fetch");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({}));
 app.use(express.json());
+
 // store contacts in an arrays
 var contacts = [
   {
@@ -34,17 +38,50 @@ var contacts = [
 ];
 
 app.get("/", function (req, res) {
-  res.send("<h1> Hello Routes: try POST to /contact and GET /contacts </h1>");
+  res.send("<h1> Goodbye Routes: try POST to /contact and GET /contacts </h1>");
 });
 // list all contacts
-app.get("/contacts", function (req, res) {
-  res.json(contacts);
-});
+
+const getData = async () => {
+  let url = "https://pollysnips.s3.amazonaws.com/users.json";
+  try {
+    let res = await fetch(url)
+    contacts = await res.json();
+    console.log(contacts);
+  } catch (error) {
+    console.log("error");
+  }
+ };
+ getData();
+
+
 // add a contact
 app.post("/contact", (req, res) => {
   contacts.push({ name: req.body.name, email: req.body.email });
   res.json(req.body);
 });
+
+app.get("/contacts", function (req, res) {
+  res.json(contacts)
+});
+
+app.get("/login", (req, res) => {
+  // send back a login form
+  let form = `<form action="/auth" method="post">
+    <label for="name">Enter name: </label>
+    <input id="name" type="text" name="name" value="name">
+    <input id="password" type="text" name="password" value="password">
+    <input type="submit" value="OK">
+    </form>`;
+  res.send(form);
+ });
+
+ app.post("/auth", (req, res) => {
+ //  send back OK you are logged in
+    let { name, password} = req.body;
+    res.send((`Hi ${name} you are logged in`));
+
+ }); 
 
 app.listen(3000);
 console.log("Running on port 3000");
